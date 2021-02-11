fd236829d3fa0f9900ca848d3ede3a0654a96653
shayaaan8 <60605667+shayaaan8@users.noreply.github.com>
2021-02-11 19:33:10 +0100

    Update RDP000.sh

--- RDP000.sh
+++ RDP000.sh
@@ -10,8 +10,7 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-sudo apt install tasksel -y
-sudo tasksel install ubuntu-desktop -y
+sudo apt install gnome-session gdm3 -y
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
