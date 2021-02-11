181cebc8071e0f26127e0ab63de57646acdb9508
shayaaan8 <60605667+shayaaan8@users.noreply.github.com>
2021-02-11 18:34:29 +0100

    Update RDP000.sh

--- RDP000.sh
+++ RDP000.sh
@@ -10,7 +10,7 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon
+sudo apt-get install ubuntu-desktop -y
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
